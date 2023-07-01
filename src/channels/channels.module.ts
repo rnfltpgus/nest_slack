56945ad76dc000ba2374feae38dddec0fca62473
rnfltpgus <jpp901220@gmail.com>
2023-07-01 15:54:54 +0900

    🔩 Feat: channels service 로직 작성

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
+import { ChannelsController } from './channels.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Channels } from '../entities/Channels';
+import { ChannelChats } from '../entities/ChannelChats';
+import { Users } from '../entities/Users';
+import { Workspaces } from '../entities/Workspaces';
+import { ChannelMembers } from '../entities/ChannelMembers';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Channels, ChannelChats, Users, Workspaces, ChannelMembers])],
+  providers: [ChannelsService],
   controllers: [ChannelsController],
-  providers: [ChannelsService]
 })
 export class ChannelsModule {}
